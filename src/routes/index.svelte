fe52a5fe98fe5c4c1c52e8ae6ef2d70f48c5dc09
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2022-03-06 10:49:28 +0100

    Upload

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Searchbar from '$lib/Searchbar.svelte';
+</script>
+
+<div class="h-full w-full flex flex-col justify-center items-center">
+	<h1 class="text-4xl font-bold text-primary">Acrys</h1>
+	<p class="mt-2">Statistical Analysis System for League of Legends</p>
+	<div class="mt-6 w-1/3">
+		<Searchbar />
+	</div>
+</div>
---
+++ src/routes/result/[id].svelte
@@ -0,0 +1,11 @@
+<script>
+	import Banner from '$lib/Banner.svelte';
+</script>
+
+<div class="w-full h-full grid grid-cols-5 gap-4 justify-evenly items-center p-4">
+	<Banner profilePictureId={20} />
+	<Banner profilePictureId={28} />
+	<Banner profilePictureId={3} />
+	<Banner profilePictureId={5} />
+	<Banner profilePictureId={10} />
+</div>
