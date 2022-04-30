bc9d26518626df11db83717f4e5f2b3b52ac86e7
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2022-04-30 20:08:04 +0200

    SSE autoreconnecting

--- src/lib/stores/query.ts
+++ src/lib/stores/query.ts
@@ -10,20 +10,36 @@ export const query: Readable<QueryStore> = derived<Writable<string>, QueryStore>
   if (browser && $queryId) {
     set(null);
 
-    const eventSource = new EventSource(`${import.meta.env.VITE_BACKEND_PATH}/query/${$queryId}/sse`);
+    let eventSource = new EventSource(`${import.meta.env.VITE_BACKEND_PATH}/query/${$queryId}/sse`);
+    let keepAliveTimer: NodeJS.Timeout | undefined;
+    getActivity();
+
+    function getActivity() {
+      if (keepAliveTimer !== null) clearTimeout(keepAliveTimer);
+      keepAliveTimer = setTimeout(reconnect, 30 * 1000);
+    }
+
+    function reconnect() {
+      eventSource.close();
+      eventSource = new EventSource(`${import.meta.env.VITE_BACKEND_PATH}/query/${$queryId}/sse`);
+    }
 
     eventSource.onmessage = (event) => {
       const data = JSON.parse(event.data);
 
       if (data.complete) {
+        if (keepAliveTimer !== null) clearTimeout(keepAliveTimer);
         eventSource.close();
+      } else {
+        getActivity();
       }
 
       set(data);
     };
 
-    return () => {
-      eventSource.close();
+    eventSource.onerror = (event) => {
+      console.log("Got error! Reconnecting...");
+      reconnect();
     };
   }
 });
