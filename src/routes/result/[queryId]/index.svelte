36ba5cfb20700d6b4a7053a3ef06088b8985b61d
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2022-05-01 20:02:14 +0200

    Remove margin if mobile

--- src/routes/result/[queryId]/index.svelte
+++ src/routes/result/[queryId]/index.svelte
@@ -11,7 +11,7 @@
 <!--<div class="inline-flex justify-start xl:(justify-center flex) items-start mx-5">-->
 <div class="flex flex-col items-center xl:(flex-row justify-center items-start) mb-10">
   {#each summoners as summoner}
-    <div class="ml-5 first:ml-0">
+    <div class="xl:ml-5 first:ml-0">
       <Banner {summoner} />
     </div>
   {/each}
